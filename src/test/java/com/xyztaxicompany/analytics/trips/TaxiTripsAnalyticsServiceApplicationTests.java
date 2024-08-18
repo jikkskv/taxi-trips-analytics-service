16f4e2c05e6f2544d302d31a0b904234c332a2fc
jikkskv <jikkskv@gmail.com>
2024-08-18 12:59:10 +0800

    [Spring] intitial commit from spring initializer

---
+++ src/test/java/com/xyztaxicompany/analytics/trips/TaxiTripsAnalyticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyztaxicompany.analytics.trips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiTripsAnalyticsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
